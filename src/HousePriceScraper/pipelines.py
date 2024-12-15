756ef30ec0f2dec5b50eb4e73a8f27961dbda25e
Railan Deivid <contato.railandeivid@gmail.com>
2024-12-15 12:55:05 -0300

    update

--- src/HousePriceScraper/pipelines.py
+++ src/HousePriceScraper/pipelines.py
@@ -8,33 +8,49 @@ load_dotenv()
 
 class PostgreSQLPipeline:
     def __init__(self):
+        # Verificar se as variáveis de ambiente estão definidas
+        self.db_host = os.getenv("DB_HOST_PROD")
+        self.db_port = os.getenv("DB_PORT_PROD")
+        self.db_name = os.getenv("DB_NAME_PROD")
+        self.db_user = os.getenv("DB_USER_PROD")
+        self.db_pass = os.getenv("DB_PASS_PROD")
+
+        if not all([self.db_host, self.db_port, self.db_name, self.db_user, self.db_pass]):
+            raise ValueError("Faltam variáveis de ambiente para a conexão com o banco de dados")
+
         # Conexão com o banco de dados usando variáveis de ambiente
-        self.conn = psycopg2.connect(
-            host=os.getenv("DB_HOST_PROD"),
-            port=os.getenv("DB_PORT_PROD"),
-            dbname=os.getenv("DB_NAME_PROD"),
-            user=os.getenv("DB_USER_PROD"),
-            password=os.getenv("DB_PASS_PROD")
-        )
-        self.cur = self.conn.cursor()
-        self.create_table()
+        try:
+            self.conn = psycopg2.connect(
+                host=self.db_host,
+                port=self.db_port,
+                dbname=self.db_name,
+                user=self.db_user,
+                password=self.db_pass
+            )
+            self.cur = self.conn.cursor()
+            self.create_table()
+        except psycopg2.Error as e:
+            raise Exception(f"Erro ao conectar com o banco de dados: {e}")
 
     def create_table(self):
         # Cria a tabela se ela não existir
-        self.cur.execute("""
-        CREATE TABLE IF NOT EXISTS house_prices (
-            id SERIAL PRIMARY KEY,
-            title TEXT,
-            price VARCHAR(20),
-            bedrooms TEXT,
-            bathrooms TEXT,
-            sqm TEXT,
-            location TEXT,
-            state VARCHAR(20),
-            source TEXT
-        )
-        """)
-        self.conn.commit()
+        try:
+            self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS house_prices (
+                id SERIAL PRIMARY KEY,
+                title TEXT,
+                price VARCHAR(20),
+                bedrooms TEXT,
+                bathrooms TEXT,
+                sqm TEXT,
+                location TEXT,
+                state VARCHAR(20),
+                source TEXT
+            )
+            """)
+            self.conn.commit()
+        except psycopg2.Error as e:
+            raise Exception(f"Erro ao criar tabela: {e}")
 
     def process_item(self, item, spider):
         try:
